0ad12c6be508a28efe47d8e9f3227b2ed9a052e1
huynguyez0705 <huy751998@gmail.com>
2025-01-06 14:57:10 +0700

    new

---
+++ notice.php
@@ -0,0 +1,26 @@
+<?php
+    	<?php include( get_template_directory() . '/breadcrumb.php' ); ?>
+<div class="notice">
+    <p>
+        Mẹo tìm phim không thấy, các bạn vui lòng tìm trên Google với cú pháp sau, 
+        <span style="color: #ff0000;"><strong>Tên Phim + 
+            <a href="<?php echo home_url(); ?>">Domain</a> 
+        </strong></span>.
+        Ví dụ tìm phim 
+        <strong><?php echo get_the_title(); ?></strong>, các bạn tìm là 
+        <strong>
+            <a href="<?php echo get_permalink(get_the_ID()); ?>">
+                <?php echo get_the_title(); ?> Domain
+            </a>.
+        </strong>
+        &nbsp;Tại <a href="<?php echo home_url(); ?>"><?php echo get_bloginfo('name'); ?></a>bạn xem không giật lag và không quảng cáo.
+    </p>
+</div>
+<style>
+.notice{background-color:#f4f6f8;border:1px solid #dfe3e8;padding:10px 15px;border-radius:5px;font-size:14px;color:#5a5a5a;margin:10px 0}
+.notice.warning{background-color:#fffaf0;border-color:#fff1cc;color:#856404}
+.notice.success{background-color:#f3fdf7;border-color:#d8f3e0;color:#155724}
+.notice.error{background-color:#fdf2f3;border-color:#f6d8da;color:#721c24}
+.notice.info{background-color:#f0f8ff;border-color:#cce7ff;color:#004085}
+</style>
+?>
