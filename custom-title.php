d5024383cb78b5aadf9ebf8e7f7e453c5a07c7a2
huynguyez0705 <huy751998@gmail.com>
2025-02-03 14:18:46 +0700

    new

--- custom-title.php
+++ custom-title.php
@@ -1,37 +1,75 @@
 <?php
 
-// Filter Title
+// rankmath
+
 add_filter('rank_math/frontend/title', function($title) {
     if (isEpisode()) {
-        $year = op_get_year(get_the_ID());
-        $originalTitle = op_get_original_title();
-        return "<strong>".get_the_title(get_the_ID())."</strong> - {(is_string($year)) ? $year : ''} - {(is_string($originalTitle)) ? $originalTitle : ''} | HHKUNGFU";
+        return sprintf(
+            'Xem phim %s (%s)- %s %s - Tập %s | HHKUNGFU',
+            get_the_title(get_the_ID()),
+	        op_get_year(get_the_ID()),
+            op_get_lang(get_the_ID()),
+            op_get_year(get_the_ID()),
+            op_get_quality(get_the_ID()),
+            episodeName()
+        );
     }
-    
     return $title;
 }, 10, 1);
-
-// Filter Description
-add_filter('rank_math/frontend/description', function($des) {
-    if (function_exists('isEpisode')) {
+add_filter( 'rank_math/frontend/description', function( $des ) {
+    if ( function_exists('isEpisode') && isEpisode() ) {
         $post_title = get_the_title(get_the_ID());
-        $original_excerpt = op_get_excerpt();
-        return "Xem phim {$post_title} | {$original_excerpt}";
+        $episode = function_exists('episodeName') ? episodeName() : '';
+        $ex = get_the_excerpt();
+        $new_des = "Xem {$post_title} Tập {$episode}. {$ex} ";
+        return $new_des;
     }
-    
     return $des;
-}, 10, 1);
+}, 10, 1 );
 
-// Filter Title using Yoast SEO
+
+
+// Yoast seo
+
 add_filter('wpseo_title', function($title) {
     if (isEpisode()) {
         $post_title = get_the_title(get_the_ID());
         $year = op_get_year(get_the_ID());
-        $originalTitle = op_get_original_title();
-        
-        return "Xem phim {$post_title} ({$year}) - {$originalTitle} | HHKUNGFU";
+        $lang = op_get_lang(get_the_ID());
+        $quality = op_get_quality(get_the_ID());
+        $episode = episodeName();
+	$ori_title = op_get_original_title();
+
+        // Tạo tiêu đề bằng nội suy chuỗi
+        $custom_title = "Xem phim {$post_title} ({$year}) - {$lang} {$quality} - Tập {$episode} | HHKUNGFU";
+
+        return $custom_title;
     }
-    
     return $title;
 }, 10, 1);
+
+// keywords
+add_action("wp_head", function() {
+    $title = get_the_title(get_the_ID());
+    $ori_title = op_get_original_title();
+    echo <<<EOT
+<meta name="keywords" content="{$title}, {$ori_title}"/>
+EOT;
+});
+
+
+// op_get_year
+function op_get_year($end ='')
+{
+    $html = "";
+    $years = get_the_terms(get_the_ID(), "ophim_years");
+    if (is_array($years)) {
+        foreach ($years as $y) {
+            if (preg_match('/^\d{4}$/', $y->name)) {
+                $html .= $y->name . $end; // Nối năm với $end nếu có
+            }
+        }
+    }
+    return $html;
+}
 ?>
