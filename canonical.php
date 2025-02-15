14655d664acbe322277abe2667f71de208bd8c3f
Bo <huy751998@gmail.com>
2025-02-16 02:53:01 +0700

    new

---
+++ canonical.php
@@ -0,0 +1,14 @@
+add_filter('wpseo_canonical', function($canonical) {
+        return 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+});
+add_filter( 'rank_math/frontend/canonical', function( $canonical ) {
+	    return 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+});
+
+// Thay đổi URL của thẻ og:url do Yoast SEO tự động tạo
+add_filter('wpseo_opengraph_url', function($og_url) {
+     return 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+});
+add_filter( 'rank_math/opengraph/url', function( $url ) {
+    return 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+});
