d7f36f5b72883a93ab1c5b1e2f01e14d2176fd27
huynguyez0705 <huy751998@gmail.com>
2025-03-26 11:51:52 +0700

    ưe

--- custom-title.php
+++ custom-title.php
@@ -2,9 +2,7 @@
 
 
 function custom_seo_title($title) {
-    if (is_admin()) {
-        return $title;  
-    } 
+     if (is_admin()) return $title;  
     if (isEpisode()) {
         $post_title = get_the_title(get_the_ID());
         $year = op_get_year(get_the_ID());
@@ -12,18 +10,17 @@ function custom_seo_title($title) {
         $quality = op_get_quality(get_the_ID());
         $episode = episodeName();
         $ori_title = op_get_original_title();
-        return "Xem phim {$post_title} ({$ori_title}) - {$lang} {$quality} - Tập {$episode} - Motchill";
+        return "Xem Phim {$post_title} - {$ori_title} - {$lang} {$quality} - Tập {$episode} - Phimmoi";
     }
     return $title;
 }
-
 
 function custom_seo_description($desc) {
     if (function_exists('isEpisode') && isEpisode()) {
         $post_title = get_the_title(get_the_ID());
         $episode = function_exists('episodeName') ? episodeName() : '';
         $ex = wp_trim_words(get_the_excerpt(), 100, '...');
-        return "Xem {$post_title} Tập {$episode}. {$ex}";
+        return "Xem Phim {$post_title} Tập {$episode}. {$ex}";
     }
     return $desc;
 }
