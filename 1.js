bf248ecb29ccfbfe4535f97f5aef99a850a9263c
huynguyez0705 <huy751998@gmail.com>
2025-07-22 16:37:18 +0700

    new

--- 1.js
+++ 1.js
@@ -1,7 +1,8 @@
-const customButton = document.querySelector('#page-info a.custom-button')
-
-customButton ? (document.querySelector('#page-info .poster .adspruce-streamlink') ? (document.querySelector('#page-info .poster .adspruce-streamlink').style.maxHeight = '350px') : null) : null
-function markPopupAsOpened(deviceType, index) {
-    localStorage.setItem("popupOpenedTime", Date.now());
-    localStorage.setItem(popupIndex_$ư{deviceType}, index);
+const a = {
+	1: '2'
 }
+const b = '1'
+console.log(a)
+
+1
+  
