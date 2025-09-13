f6e7e9301a8f2f582019f587225fc98eff129fdc
Bo <huy751998@gmail.com>
2025-09-14 02:39:26 +0700

    new

--- Breadcrumb/breadcrumb-3.php
+++ Breadcrumb/breadcrumb-3.php
@@ -1,20 +1,24 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 <?php
-function breadcrumb_home() {
+function breadcrumb_home()
+{
 	$name = 'Motchill';
 	$url = home_url();
 ?>
 <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
-	<a itemprop="item" href='<?php echo esc_url($url); ?>'>
-		<i class="fa fa-home"></i> 
-		<span itemprop="name"><?php echo esc_html($name); ?></span>
-	</a>
-	<meta itemprop="position" content="1" />
-	<i class="fa fa-long-arrow-alt-right"></i>
+  <a itemprop="item" href='<?php echo esc_url($url); ?>'>
+    <i class="fa fa-home"></i>
+    <span itemprop="name"><?php echo esc_html($name); ?></span>
+  </a>
+  <meta itemprop="position" content="1" />
+  <i class="fa fa-caret-right"></i>
+
 </li>
 <?php } ?>
 
 <?php
-function breadcrumb_cate() {
+function breadcrumb_cate()
+{
 	breadcrumb_home();
 	if (isEpisode() || (is_single() && get_post_type() === 'ophim')) {
 		$categories_episode = get_the_terms(get_the_ID(), 'ophim_categories');
@@ -22,15 +26,15 @@ function breadcrumb_cate() {
 		$url_cate_epis = $categories_episode && !is_wp_error($categories_episode) ? get_term_link($categories_episode[0]) : '#';
 ?>
 <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
-	<a itemprop="item" href='<?php echo esc_url($url_cate_epis); ?>'>
-		<span itemprop="name"><?php echo esc_html($name_cate_epis); ?></span>
-	</a>
-	<meta itemprop="position" content="2" />
-	<i class="fa fa-long-arrow-alt-right"></i>
+  <a itemprop="item" href='<?php echo esc_url($url_cate_epis); ?>'>
+    <span itemprop="name"><?php echo esc_html($name_cate_epis); ?></span>
+  </a>
+  <meta itemprop="position" content="2" />
+  <i class="fa fa-caret-right"></i>
 </li>
-<?php 
+<?php
 	} else {
-		$taxonomies = ['ophim_categories'=>'','ophim_directors'=>'Đạo Diễn','ophim_years'=>'Năm Phát Hành','ophim_actors'=>'Diễn Viên','ophim_regions'=>'Quốc Gia','ophim_genres'=>'Thể Loại'];
+		$taxonomies = ['ophim_categories' => '', 'ophim_directors' => 'Đạo Diễn', 'ophim_years' => 'Năm Phát Hành', 'ophim_actors' => 'Diễn Viên', 'ophim_regions' => 'Quốc Gia', 'ophim_genres' => 'Thể Loại'];
 		$cate = get_queried_object()->taxonomy;
 		$cate_name = single_tag_title('', false);
 		$prefix = $taxonomies[$cate] ?? '';
@@ -38,18 +42,18 @@ function breadcrumb_cate() {
 		$paged = get_query_var('paged') ?: 1;
 		$paged_url = add_query_arg('paged', $paged, $cate_url);
 		$current_url = esc_url('https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
-?>
+	?>
 <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
-	<meta itemprop="item" content='<?php echo esc_url($cate_url); ?>'>
-	<span itemprop="name"><?php echo esc_html($cate_name); ?></span>
-	<meta itemprop="position" content="2" />
-	<?php if ($paged > 1) { ?> <i class="fa fa-long-arrow-alt-right"></i> <?php } ?>
+  <meta itemprop="item" content='<?php echo esc_url($cate_url); ?>'>
+  <span class="breadcrumb_last" itemprop="name"><?php echo esc_html($cate_name); ?></span>
+  <meta itemprop="position" content="2" />
+  <?php if ($paged > 1) { ?> <i class="fa fa-caret-right"></i> <?php } ?>
 </li>
 <?php if ($paged > 1) { ?>
 <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
-	<meta itemprop="item" content='<?php echo esc_url($current_url); ?>'>
-	<span itemprop="name">Trang <?php echo $paged; ?></span>
-	<meta itemprop="position" content="3" />
+  <meta itemprop="item" content='<?php echo esc_url($current_url); ?>'>
+  <span itemprop="name">Trang <?php echo $paged; ?></span>
+  <meta itemprop="position" content="3" />
 </li>
 <?php }
 	}
@@ -57,7 +61,8 @@ function breadcrumb_cate() {
 ?>
 
 <?php
-function breadcrumb_single() { 
+function breadcrumb_single()
+{
 	breadcrumb_cate();
 	global $post;
 	$title = get_the_title($post->ID);
@@ -65,31 +70,33 @@ function breadcrumb_single() {
 	if (isEpisode()) {
 ?>
 <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
-	<a itemprop="item" href='<?php echo esc_url($url_post); ?>'>
-		<span itemprop="name"><?php echo esc_html($title); ?></span>
-	</a>
-	<meta itemprop="position" content="3" />
-	<i class="fa fa-long-arrow-alt-right"></i>
+  <a itemprop="item" href='<?php echo esc_url($url_post); ?>'>
+    <span itemprop="name"><?php echo esc_html($title); ?></span>
+  </a>
+  <meta itemprop="position" content="3" />
+  <i class="fa fa-caret-right"></i>
 </li>
 <?php } else { ?>
 <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
-	<meta itemprop="item" content='<?php echo esc_url($url_post); ?>'>
-	<span itemprop="name"><?php echo esc_html($title); ?></span>
-	<meta itemprop="position" content="3" />
+  <meta itemprop="item" content='<?php echo esc_url($url_post); ?>'>
+  <span clas="breadcrumb_last" itemprop="name"><?php echo esc_html($title); ?></span>
+  <meta itemprop="position" content="3" />
 </li>
-<?php } } ?>
+<?php }
+} ?>
 
 <?php
-function breadcrumb_episode() {
-    breadcrumb_single();
-    global $post;
-    $title_epis = 'Tập ' . episodeName();
-    $url_epis = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+function breadcrumb_episode()
+{
+	breadcrumb_single();
+	global $post;
+	$title_epis = 'Tập ' . episodeName();
+	$url_epis = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 ?>
 <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
-    <meta itemprop="item" content='<?php echo esc_url($url_epis); ?>'>
-    <span itemprop="name"><?php echo esc_html($title_epis); ?></span>
-    <meta itemprop="position" content="4" />
+  <meta itemprop="item" content='<?php echo esc_url($url_epis); ?>'>
+  <span clas="breadcrumb_last" itemprop="name"><?php echo esc_html($title_epis); ?></span>
+  <meta itemprop="position" content="4" />
 </li>
 <?php } ?>
 <!-- Render -->
@@ -97,48 +104,90 @@ function breadcrumb_episode() {
 <?php if (isEpisode()) { ?>
 <!-- Trang Episode -->
 <ul class="breadcrumb" itemscope itemtype="https://schema.org/BreadcrumbList">
-    <?php breadcrumb_episode(); ?>
+  <?php breadcrumb_episode(); ?>
 </ul>
 <?php } elseif (is_single() && get_post_type() === 'ophim' && !isEpisode()) { ?>
 <!-- Trang Info -->
 <ul class="breadcrumb" itemscope itemtype="https://schema.org/BreadcrumbList">
-    <?php breadcrumb_single(); ?>
+  <?php breadcrumb_single(); ?>
 </ul>
 <?php } ?>
 
 <!-- <?php $taxonomy_list = array('ophim_categories', 'ophim_actors', 'ophim_genres', 'ophim_regions', 'ophim_tags', 'ophim_years', 'ophim_directors'); ?>
  -->
-<?php if (is_tax($taxonomy_list)) { 
-    $url_archive = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+<?php if (is_tax($taxonomy_list)) {
+	$url_archive = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 ?>
 <!-- Trang Taxonomy -->
 <ul class="breadcrumb" itemscope itemtype="https://schema.org/BreadcrumbList">
-	<?php breadcrumb_cate(); ?>
+  <?php breadcrumb_cate(); ?>
 </ul>
 <?php } ?>
 <?php if (is_search()) { ?>
 <!--  Trang Search -->
 <ul class="breadcrumb" itemscope itemtype="https://schema.org/BreadcrumbList">
-    <?php breadcrumb_home(); ?>
-    <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
-        <meta itemprop="item" content='<?php echo esc_url(home_url('/?s=' . urlencode(get_search_query()))); ?>'>
-        <span itemprop="name">Kết quả tìm kiếm: <?php echo esc_html(get_search_query()); ?></span>
-        <meta itemprop="position" content="2" />
-    </li>
+  <?php breadcrumb_home(); ?>
+  <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+    <meta itemprop="item" content='<?php echo esc_url(home_url('/?s=' . urlencode(get_search_query()))); ?>'>
+    <span itemprop="name">Kết quả tìm kiếm: <?php echo esc_html(get_search_query()); ?></span>
+    <meta itemprop="position" content="2" />
+  </li>
 </ul>
 <?php } ?>
-<?php if (is_archive() && !is_tax($taxonomy_list)) { 
-$url_archive = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+<?php if (is_archive() && !is_tax($taxonomy_list)) {
+	$url_archive = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 ?>
 <!--  Trang Archive -->
 <ul class="breadcrumb" itemscope itemtype="https://schema.org/BreadcrumbList">
-    <?php breadcrumb_home(); ?>
-    <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
-        <meta itemprop="item" content='<?php echo esc_url($url_archive) ?>'>
-        <span itemprop="name">Kho Phim MotPhimTV</span>
-        <meta itemprop="position" content="2" />
-    </li>
+  <?php breadcrumb_home(); ?>
+  <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+    <meta itemprop="item" content='<?php echo esc_url($url_archive) ?>'>
+    <span itemprop="name">Kho Phim MotPhimTV</span>
+    <meta itemprop="position" content="2" />
+  </li>
 </ul>
 <?php } ?>
-<style>.breadcrumb {color:#fff;;display: flex;gap: 10px;padding: 1rem 0 2rem;}
-.breadcrumb a {color:#c58560; margin-right:10px}</style>
+<style>
+:root {
+  --color: #da966e;
+  --color-hover: #b37a58;
+  --color-focus: #e46d2b;
+  --bs-body-font-system-ui: system-ui, -apple-system, 'Segoe UI', Roboto, 'Helvetica Neue', 'Noto Sans',
+    'Liberation Sans', Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
+  --bs-body-font-nunito: 'Nunito';
+  --text-base: #abb7c4;
+}
+
+.breadcrumb {
+  color: var(--text-base);
+  display: flex;
+  gap: 10px;
+  padding: 1.2rem 10px;
+  background-color: #181818;
+  font-size: 1rem;
+  margin: 1rem 10px;
+  font-size: 14px;
+}
+
+.breadcrumb i {
+  color: var(--color);
+}
+
+.breadcrumb a {
+  color: var(--color);
+  margin-right: 10px
+}
+
+.breadcrumb li {
+  display: flex;
+  align-items: center;
+}
+
+.breadcrumb>li+li:before {
+  content: none
+}
+
+.breadcrumb .breadcrumb_last {
+  color: #fff
+}
+</style>
